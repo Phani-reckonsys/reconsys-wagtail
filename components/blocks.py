6e5df6a4082e28a33c4ebba67e80423f7964fd20
Phani-reckonsys <phani@reckonsys.com>
2024-04-09 19:10:43 +0530

    custom blocks

--- components/blocks.py
+++ components/blocks.py
@@ -4,13 +4,22 @@ from wagtail.blocks import (
     RichTextBlock,
     StreamBlock,
     StructBlock,
-    BooleanBlock
+    BooleanBlock,
+    PageChooserBlock
 )
 from wagtail.models import StreamField
 from wagtail.embeds.blocks import EmbedBlock
 from wagtail.images.blocks import ImageChooserBlock
 
+class PrimaryButtonBlock(StructBlock): 
+    title = CharBlock(classname = "title", required = True)
+    icon = ImageChooserBlock(required = False)
+    link = PageChooserBlock(can_choose_root= True)
 
+    class Meta:
+        min_num = 0
+        max_num = 1
+        template = "basic_blocks/primary_button.html"
 
 class HeroTitleBlock(StructBlock):
     title = CharBlock(classname="title", required=True)
@@ -19,7 +28,11 @@ class HeroTitleBlock(StructBlock):
 class HeroSectionBlock(StructBlock):
     titles = StreamBlock([('title', HeroTitleBlock())])
     include_quick_testimonials = BooleanBlock(required=False)
-
+    primary_button = StreamBlock([('button', PrimaryButtonBlock())])
+    image_cover = ImageChooserBlock(required=False)
+    image_decor = ImageChooserBlock(required= False)
     class Meta:
         icon = "title"
         template = "blocks/herosection.html"
+
+
