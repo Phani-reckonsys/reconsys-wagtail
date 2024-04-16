c7639dc59b5fa218c9586de588e4465cd8fed2fd
Phani-reckonsys <phani@reckonsys.com>
2024-04-16 15:44:14 +0530

    our work display card

--- components/models.py
+++ components/models.py
@@ -8,9 +8,10 @@ class Navbar(ClusterableModel):
     Home_page_link = models.ForeignKey("wagtailcore.Page", null = True, blank = True, on_delete = models.SET_NULL, related_name = "+")
     Contact_page = models.CharField(max_length = 255, blank=True, help_text = "contactus Link")
     Contact_page_link = models.ForeignKey("wagtailcore.Page", null = True, blank = True, on_delete = models.SET_NULL, related_name = "+")
+    bgcolor = models.CharField(max_length = 255, blank=True, help_text = "contactus Link")
 
     # navbar content variables
-    panels = [InlinePanel('navitems', heading='navbar'), FieldPanel("Contact_page"), PageChooserPanel("Contact_page_link"), PageChooserPanel("Home_page_link")]
+    panels = [InlinePanel('navitems', heading='navbar'), FieldPanel("Contact_page"), FieldPanel("bgcolor"), PageChooserPanel("Contact_page_link"), PageChooserPanel("Home_page_link")]
 
 class NavItem(Orderable, models.Model):
     name = models.CharField(max_length = 255, blank=True, help_text = "Navigation link")
