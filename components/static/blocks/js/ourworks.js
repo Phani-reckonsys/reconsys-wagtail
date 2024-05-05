d3c2388a414240aa5f2e47f1a314eb4d20ffa02a
Phani-reckonsys <phani@reckonsys.com>
2024-05-05 15:04:52 +0530

    ouworks cards sorting

--- components/static/blocks/js/ourworks.js
+++ components/static/blocks/js/ourworks.js
@@ -1,7 +1,7 @@
 const showMoreBtn = document.querySelector(".ourwork-display .outline-btn");
 const showMoreBtnText = document.querySelector(".ourwork-display .outline-btn h4");
 let ourWorkCardVisibleCount = 8;
-const ourWorkCards = document.querySelectorAll(".our-works-card");
+const ourWorkCards = document.querySelectorAll(".our-works-card");  // All cards selector
 function setOurWorksCardCount(count) {
   ourWorkCardVisibleCount = count;
   for (const [index, card] of ourWorkCards.entries()) {
@@ -22,3 +22,45 @@ showMoreBtn.addEventListener("click", () => {
   if (ourWorkCardVisibleCount > 20) setOurWorksCardCount(8);
   else setOurWorksCardCount(50);
 });
+
+const allBtn = document.querySelector(".all-btn");
+const csdBtn = document.querySelector(".csd-btn");
+const uiBtn = document.querySelector(".ui-btn");
+
+const csdCards = document.querySelectorAll(".our-works-card.csd");  // All cards selector
+const uiCards = document.querySelectorAll(".our-works-card.ui");  // All cards selector
+
+allBtn.addEventListener('click', () => {
+  ourWorkCards.forEach(card => {
+    card.classList.remove("none");
+    card.classList.remove("active");
+  });
+});
+
+csdBtn.addEventListener('click', () => {
+  ourWorkCards.forEach(card => {
+    card.classList.add("none");
+  });
+  uiCards.forEach(card => {
+    card.classList.add("none");
+  });
+  csdCards.forEach(card => {
+    card.classList.remove("none");
+    card.classList.add("active");
+  });
+});
+
+uiBtn.addEventListener('click', () => {
+  ourWorkCards.forEach(card => {
+    card.classList.add("none");
+  });
+  uiCards.forEach(card => {
+    card.classList.remove("none");
+    card.classList.add("active");
+  });
+  csdCards.forEach(card => {
+    
+    card.classList.add("none");
+  });
+});
+
