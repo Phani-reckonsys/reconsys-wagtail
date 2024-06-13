96ccd5d5473b2d93ce4c4bd36e6fcc7cbf096135
Phani-reckonsys <phani@reckonsys.com>
2024-06-13 11:05:51 +0530

    setting time out for welcomeback model

--- components/static/blocks/js/welcomebackmodel.js
+++ components/static/blocks/js/welcomebackmodel.js
@@ -1,18 +1,23 @@
-const welcomebackCloseBtn = document.querySelector(".welcomeback-close-btn");
-const welcomebackContainer = document.querySelector(".body-container-welcomeback-model");
+function delayedExecution() {
+    const welcomebackCloseBtn = document.querySelector(".welcomeback-close-btn");
+    const welcomebackContainer = document.querySelector(".body-container-welcomeback-model");
 
-document.addEventListener('mouseout', function(event) {
-    if (event.clientY <= 0) {
-    if (!document.cookie.split(';').some((item) => item.trim().startsWith('seenMessage='))) {
-        // Cookie does not exist, show the message
-        welcomebackContainer.classList.remove('remove');
-    } 
-    }
-});
+    document.addEventListener('mouseout', function(event) {
+        if (event.clientY <= 0) {
+            if (!document.cookie.split(';').some((item) => item.trim().startsWith('seenMessage='))) {
+                // Cookie does not exist, show the message
+                welcomebackContainer.classList.remove('remove');
+            } 
+        }
+    });
 
-welcomebackCloseBtn.addEventListener("click", () => {
-    welcomebackContainer.classList.add("remove");
-    // Set a cookie named 'seenMessage' that expires in 30 days
-    document.cookie = 'seenMessage=true; expires=' + new Date(Date.now() + 2 * 60 * 60 * 1000).toUTCString() + '; path=/';
-});
+    welcomebackCloseBtn.addEventListener("click", () => {
+        welcomebackContainer.classList.add("remove");
+        // Set a cookie named 'seenMessage' that expires in 30 days
+        document.cookie = 'seenMessage=true; expires=' + new Date(Date.now() + 2 * 60 * 60 * 1000).toUTCString() + '; path=/';
+    });
 
+}
+
+// Call the function after 1 minute of page load
+setTimeout(delayedExecution, 6000); // 1 minute in milliseconds
