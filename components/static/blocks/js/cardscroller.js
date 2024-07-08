20b5ace5dbcb77f913ae4f68f04b31e432f419ee
Phani-reckonsys <phani@reckonsys.com>
2024-07-08 12:54:01 +0530

    cardscroller.js

--- components/static/blocks/js/cardscroller.js
+++ components/static/blocks/js/cardscroller.js
@@ -1,45 +1,58 @@
-let check = document.querySelector(".right");
-if (check && check.id === "active"){
-let sectorCardInterval;
-const sectorCardWrapper = document.querySelector(".cardscroller-body");
-const sectorCards = document.querySelector(".engagement-cards-wrapper").children;
-
-let currentSectorCardIndex = 0;
-
-const scrollToSectorCard = (index) => {
-  // Make sure the index does not go out of bounds.
-  // Adding [sectorCards.length] because of js modulo giving negative.
-  index = (index + sectorCards.length) % sectorCards.length;
-  currentSectorCardIndex = index;
-
-  const scrollLeftValue =
-    sectorCards[index].offsetLeft - sectorCardWrapper.offsetLeft;
-
-  sectorCardWrapper.scrollTo({ left: scrollLeftValue, behavior: "smooth" });
-};
-startSectorCardInterval();
-const sectorCardsLeftButton = document.getElementById(
-  "cardscroller-left-arrow"
-);
-sectorCardsLeftButton.addEventListener("click", () => {
-  scrollToSectorCard(currentSectorCardIndex - 1);
-  stopSectorCardInterval();
-  startSectorCardInterval();
-});
-const sectorCardsRightButton = document.getElementById(
-  "cardscroller-right-arrow"
-);
-sectorCardsRightButton.addEventListener("click", () => {
-  scrollToSectorCard(currentSectorCardIndex + 1);
-  stopSectorCardInterval();
-  startSectorCardInterval();
-});
-function startSectorCardInterval(){
-  sectorCardInterval = setInterval(() => {
-  scrollToSectorCard(currentSectorCardIndex + 1);
-}, 5000);
-}
-function stopSectorCardInterval(){
-  clearInterval(sectorCardInterval);
-}
-}
+document.addEventListener("DOMContentLoaded", () => {
+    const checks = document.querySelectorAll(".right");
+    
+    checks.forEach((check) => {
+      if (check.classList.contains("active")) {
+        console.log("working");
+  
+        let sectorCardInterval;
+        const sectorCardWrapper = document.querySelector(".cardscroller-body");
+        const sectorCards = document.querySelector(".engagement-cards-wrapper").children;
+  
+        let currentSectorCardIndex = 0;
+  
+        const scrollToSectorCard = (index) => {
+          // Make sure the index does not go out of bounds.
+          // Adding [sectorCards.length] because of js modulo giving negative.
+          index = (index + sectorCards.length) % sectorCards.length;
+          currentSectorCardIndex = index;
+  
+          const scrollLeftValue =
+            sectorCards[index].offsetLeft - sectorCardWrapper.offsetLeft;
+  
+          sectorCardWrapper.scrollTo({ left: scrollLeftValue, behavior: "smooth" });
+        };
+  
+        const startSectorCardInterval = () => {
+          sectorCardInterval = setInterval(() => {
+            scrollToSectorCard(currentSectorCardIndex + 1);
+          }, 5000);
+        };
+  
+        const stopSectorCardInterval = () => {
+          clearInterval(sectorCardInterval);
+        };
+  
+        startSectorCardInterval();
+  
+        const sectorCardsLeftButton = document.querySelector(".carousal-outline-btn-left");
+        if (sectorCardsLeftButton) {
+          sectorCardsLeftButton.addEventListener("click", () => {
+            scrollToSectorCard(currentSectorCardIndex - 1);
+            stopSectorCardInterval();
+            startSectorCardInterval();
+          });
+        }
+  
+        const sectorCardsRightButton = document.querySelector(".carousal-outline-btn-right");
+        if (sectorCardsRightButton) {
+          sectorCardsRightButton.addEventListener("click", () => {
+            scrollToSectorCard(currentSectorCardIndex + 1);
+            stopSectorCardInterval();
+            startSectorCardInterval();
+          });
+        }
+      }
+    });
+  });
+  
